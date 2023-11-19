ec64fbda4fc7f3b2efe7ba45ebd7288365cec4ca
Nworm <xiaosi1574@gmail.com>
2023-11-19 17:00:56 +0800

    🚧 (r/pages): 优化 404 页面

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,8 +3,11 @@ import { Link, HeadFC, PageProps } from 'gatsby'
 
 const NotFoundPage: React.FC<PageProps> = () => {
     return (
-        <div>
-            <Link to='/'>index</Link>
+        <div className='h-screen w-full flex flex-col justify-center'>
+            <div className='text-center'>
+                <h1 className='mb-4 text-9xl font-bold font-mono text-center text-gray-600'>404</h1>
+                <Link className='text-gray-700 hover:text-blue-500' to='/'>返回主页</Link>
+            </div>
         </div>
     )
 }
